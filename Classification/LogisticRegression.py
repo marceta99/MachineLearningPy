4d84e1bb952c03f7511cb4ef450446a7c1ec5541
Mihailo Marcetic <marcetaking@gmail.com>
2024-03-14 20:31:18 +0100

    Classification

---
+++ Classification/LogisticRegression.py
@@ -0,0 +1,65 @@
+# Logistic Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+print(X_train)
+print(y_train)
+print(X_test)
+print(y_test)
+
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+print(X_train)
+print(X_test)
+
+# Training the Logistic Regression model on the Training set
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train, y_train)
+
+# Predicting a new result for customer that is 30 years old and has salary 87000
+print(classifier.predict(sc.transform([[30,87000]])))
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+#compare predicted values and real values from test set
+print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+# accuracy of our model, here is 89% so it means that we predicted 89% correct predictions
+print(accuracy_score(y_test, y_pred))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
